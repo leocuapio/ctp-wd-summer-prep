10e5a86809f13723c0312d5be2d4249097059566
Leonardo Cuapio <165205677+leocuapio@users.noreply.github.com>
2025-08-22 22:38:34 -0400

    Pull Request

--- habit-tracker/C11/leocuapio/template/src/main.js
+++ habit-tracker/C11/leocuapio/template/src/main.js
@@ -1,6 +1,6 @@
 console.log("test")
 
-let habits = []
+let habits = JSON.parse(localStorage.getItem('habits') || '[]');
 
 const form = document.getElementById('habit_form')
 
@@ -15,17 +15,35 @@ form.addEventListener('submit', (event) => {
     }
 
     habits.push(habit)
+    savedHabits()
     console.log(habits)
     renderHabits(habits)
 })
 
+let saved_habits = JSON.parse(localStorage.getItem('habits'))
+console.log(saved_habits)
+
 const renderHabits = (habits) => {
     const habitList = document.getElementById('habit_list')
-
+    habitList.innerHTML = ''; 
     for (let i = 0; i < habits.length; i++) {
         const habit = habits[i]
         const li = document.createElement('li')
         li.textContent = `${habit.name} Target Streak: ${habit.targetStreak}`
         habitList.appendChild(li)
     }
+}
+renderHabits(habits);
+
+document.getElementById("deleteBtn").addEventListener("click", () => {
+    const Habit_To_Del = prompt("Enter the habit you want to delete: ") 
+    if (Habit_To_Del) {
+        habits = habits.filter(h => h.name !== Habit_To_Del.trim())
+        savedHabits()
+        renderHabits(habits)
+    }
+})
+
+function savedHabits() {
+    localStorage.setItem('habits', JSON.stringify(habits))
 }
